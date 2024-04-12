611d0a361bd1a9f84c2e5ae6003aa062dfb36e22
Ernest <estevens612@yahoo.com>
2024-04-11 22:16:16 -0500

    Update crud.sql

--- crud.sql
+++ crud.sql
@@ -5,8 +5,8 @@
 -- Add a new customer
 INSERT INTO Customers (FirstName, LastName, Email, PhoneNumber) VALUES ('Alice', 'Smith', 'alicesmith@example.com', '555-6789');
 
--- Add a new bike
-INSERT INTO Bikes (Model, Status, LastMaintenanceDate, LocationID) VALUES ('Electric 500', 'Available', '2023-03-30', 1);
+-- Add a new bike using StatusID
+INSERT INTO Bikes (Model, StatusID, LastMaintenanceDate) VALUES ('Electric 500', (SELECT StatusID FROM Statuses WHERE StatusDescription = 'Available'), '2023-03-30');
 
 -- Add a new rental
 INSERT INTO Rentals (CustomerID, BikeID, RentalStartDate, RentalEndDate, Price) VALUES (3, 3, '2023-04-05', '2023-04-07', 49.99);
@@ -19,8 +19,10 @@ INSERT INTO Locations (Address, City, State, ZipCode) VALUES ('101 Cedar Rd', 'R
 -- Get all customers
 SELECT * FROM Customers;
 
--- Find available bikes at a specific location
-SELECT * FROM Bikes WHERE Status = 'Available' AND LocationID = 1;
+-- Find available bikes at a specific location using StatusID
+SELECT Bikes.*, Statuses.StatusDescription FROM Bikes
+JOIN Statuses ON Bikes.StatusID = Statuses.StatusID
+WHERE Statuses.StatusDescription = 'Available' AND LocationID = 1;
 
 -- Get rental details for a specific customer
 SELECT * FROM Rentals WHERE CustomerID = 1;
@@ -30,8 +32,8 @@ SELECT * FROM Rentals WHERE CustomerID = 1;
 -- Update a customer's phone number
 UPDATE Customers SET PhoneNumber = '555-9876' WHERE CustomerID = 1;
 
--- Change a bike's status to 'Rented'
-UPDATE Bikes SET Status = 'Rented' WHERE BikeID = 1;
+-- Change a bike's status to 'Rented' using StatusID
+UPDATE Bikes SET StatusID = (SELECT StatusID FROM Statuses WHERE StatusDescription = 'Rented') WHERE BikeID = 1;
 
 -- Extend a rental's end date
 UPDATE Rentals SET RentalEndDate = '2023-04-09' WHERE RentalID = 1;
